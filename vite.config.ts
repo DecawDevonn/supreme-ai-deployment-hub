dde483d3ece30eaa506788e4885923136015bd58
rajeshannam <annam.rajesh@softsuave.org>
2025-10-31 23:00:04 +0530

    Updated code for Azure pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,6 +15,19 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+     viteStaticCopy({
+      targets: [
+        { src: "manifest.json", dest: "." },
+        { src: "background.js", dest: "." },
+        { src: "popup.html", dest: "." },
+        { src: "popup.js", dest: "." },
+        { src: "popup.css", dest: "." },
+        { src: "settings.html", dest: "." },
+        { src: "settings.js", dest: "." },
+        { src: "settings.css", dest: "." },
+        { src: "icons/**/*", dest: "icons" },  // This will copy everything inside "icons/"
+      ],
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
